5e53c4b4790eccce25824754ffb9009e934b7ba3
jwkriewallLW <john.kriewall@lightwellinc.com>
2023-10-31 16:01:41 -0400

    Icons added to profile

--- lightwell-social-experiment/src/components/ProfileInformationComponent/ProfileInformationComponent.tsx
+++ lightwell-social-experiment/src/components/ProfileInformationComponent/ProfileInformationComponent.tsx
@@ -5,7 +5,7 @@ import { useAppSelector } from '../../app/hooks/hooks';
 import { useEffect } from 'react';
 import { User } from '../../models/ProfileModel';
 import styled from '@emotion/styled';
-import { Button, Modal } from '@mui/material';
+import { Button, Modal, useMediaQuery } from '@mui/material';
 import React from 'react';
 import UserRegistrationForm from '../LoginComponent/UserRegistrationForm';
 import Media from '../MediaComponent/Media';
@@ -14,6 +14,8 @@ import { editUserTheme } from '../../api/UserApi';
 import { FormControlLabel, Switch, FormGroup } from "@mui/material";
 import { useTheme } from '@mui/material/styles';
 import EditIcon from '@mui/icons-material/Edit';
+import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
+import AdUnitsIcon from '@mui/icons-material/AdUnits';
 
 import dayjs from "dayjs";
 
@@ -34,96 +36,97 @@ export default function ProfileInformationComponent(props: User) {
 
     const state: any = store.getState();
 
-	const [themeColor, setThemeColor] = React.useState<String>(state?.user?.profile.theme || "light");
-	const [checked, setChecked] = React.useState<boolean>(state.user.profile.theme === "light" ? false : true);
+    const [themeColor, setThemeColor] = React.useState<String>(state?.user?.profile.theme || "light");
+    const [checked, setChecked] = React.useState<boolean>(state.user.profile.theme === "light" ? false : true);
     const theme = useTheme();
     const user: User = useAppSelector(state => state.user.profile);
     const [open, setOpen] = React.useState(false);
     const [openImage, setOpenImage] = React.useState(false);
     const [openBanner, setOpenBanner] = React.useState(false);
-    
+    const isMobile = useMediaQuery('(max-width:600px)');
+
     const MaterialUISwitch = styled(Switch)(() => ({
-    width: 62,
-    height: 34,
-    padding: 7,
-    '& .MuiSwitch-switchBase': {
-        margin: 1,
-        padding: 0,
-        transform: 'translateX(6px)',
-        '&.Mui-checked': {
-        color: '#fff',
-        transform: 'translateX(22px)',
-        '& .MuiSwitch-thumb:before': {
-            backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-            '#fff',
-            )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+        width: 62,
+        height: 34,
+        padding: 7,
+        '& .MuiSwitch-switchBase': {
+            margin: 1,
+            padding: 0,
+            transform: 'translateX(6px)',
+            '&.Mui-checked': {
+                color: '#fff',
+                transform: 'translateX(22px)',
+                '& .MuiSwitch-thumb:before': {
+                    backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+                        '#fff',
+                    )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+                },
+                '& + .MuiSwitch-track': {
+                    opacity: 1,
+                    backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+                },
+            },
         },
-        '& + .MuiSwitch-track': {
+        '& .MuiSwitch-thumb': {
+            backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
+            width: 32,
+            height: 32,
+            '&:before': {
+                content: "''",
+                position: 'absolute',
+                width: '100%',
+                height: '100%',
+                left: 0,
+                top: 0,
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+                    '#fff',
+                )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+            },
+        },
+        '& .MuiSwitch-track': {
             opacity: 1,
             backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+            borderRadius: 20 / 2,
         },
-        },
-    },
-    '& .MuiSwitch-thumb': {
-        backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
-        width: 32,
-        height: 32,
-        '&:before': {
-        content: "''",
-        position: 'absolute',
-        width: '100%',
-        height: '100%',
-        left: 0,
-        top: 0,
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-            '#fff',
-        )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
-        },
-    },
-    '& .MuiSwitch-track': {
-        opacity: 1,
-        backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
-        borderRadius: 20 / 2,
-    },
     }));
 
     const userCanEdit: boolean = props._id === user._id;
 
     function setThemeOfUser() {
-		if(state?.user?.profile.theme !== undefined) {
-			setThemeColor(state.user.profile.theme);
-		}
-	}
+        if (state?.user?.profile.theme !== undefined) {
+            setThemeColor(state.user.profile.theme);
+        }
+    }
 
     function reverseTheme(theme: String) {
-		if (theme === "light") {
-			setChecked(true)
-			return "dark";
-		} else {
-			setChecked(false)
-			return "light";
-		}
-	}
+        if (theme === "light") {
+            setChecked(true)
+            return "dark";
+        } else {
+            setChecked(false)
+            return "light";
+        }
+    }
 
-	const toggleTheme = () => {
-		setThemeColor((currentTheme) => currentTheme === 'light' ? 'dark' : 'light')
+    const toggleTheme = () => {
+        setThemeColor((currentTheme) => currentTheme === 'light' ? 'dark' : 'light')
 
-		if(state.user !== null) {
-			let user: User = {...state.user.profile};
-			user.theme = reverseTheme(themeColor);
+        if (state.user !== null) {
+            let user: User = { ...state.user.profile };
+            user.theme = reverseTheme(themeColor);
 
-			const action = editUserTheme(user)
-			console.log("Inside the toggleTheme function. Current theme: " + user.theme)
-			console.log(user) 
-			store.dispatch(action);
-		}
-	}
+            const action = editUserTheme(user)
+            console.log("Inside the toggleTheme function. Current theme: " + user.theme)
+            console.log(user)
+            store.dispatch(action);
+        }
+    }
 
-	useEffect(() => {
-		setThemeOfUser();
-	}, [])
+    useEffect(() => {
+        setThemeOfUser();
+    }, [])
 
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
@@ -148,69 +151,72 @@ export default function ProfileInformationComponent(props: User) {
                 </div>
             </Box>
 
-        {userCanEdit &&
-            <div className={ProfileInformationComponentStyle.editButtons}>
-                <div>
-		    <FormGroup>
-			<FormControlLabel
-                                control={<MaterialUISwitch onChange={toggleTheme} defaultChecked={checked} sx={{ marginLeft: 1, marginTop: 1 }} />}
-				label=""
-				/>
-			</FormGroup>
-        </div>
-                <Button variant="outlined" sx={{ marginLeft: 1, marginTop: 1 }} onClick={handleOpen} className={ProfileInformationComponentStyle.editButton}>
-                    <div className={ProfileInformationComponentStyle.editIcon}><EditIcon /></div>
-                    Edit Profile
-                </Button>
-                {open &&
-                    <Modal
-                        open={open}
-                        onClose={handleClose}
-                        aria-labelledby="modal-modal-title"
-                        aria-describedby="modal-modal-description"
-                    >
-                        <Box sx={style}>
-                            Update Profile
-                            <UserRegistrationForm profileStatus="edit" onClose={handleClose} />
-                        </Box>
-                    </Modal>
-                }
+            {userCanEdit &&
+                <div className={ProfileInformationComponentStyle.editButtons}>
+                    <div>
+                        <FormGroup>
+                            <FormControlLabel
+                                control={<MaterialUISwitch onChange={toggleTheme}
+                                    defaultChecked={checked} sx={{ marginLeft: 1, marginTop: 1 }} />}
+                                label=""
+                            />
+                        </FormGroup>
+                    </div>
+                    <Button variant="outlined" sx={{ marginLeft: 1, marginTop: 1 }} onClick={handleOpen} className={ProfileInformationComponentStyle.editButton}>
+                        <div className={ProfileInformationComponentStyle.editIcon}><EditIcon /></div>
+                        {!isMobile && 'Edit Profile'}
+                    </Button>
+                    {open &&
+                        <Modal
+                            open={open}
+                            onClose={handleClose}
+                            aria-labelledby="modal-modal-title"
+                            aria-describedby="modal-modal-description"
+                        >
+                            <Box sx={style}>
+                                Update Profile
+                                <UserRegistrationForm profileStatus="edit" onClose={handleClose} />
+                            </Box>
+                        </Modal>
+                    }
 
-                <Button variant="outlined" sx={{ marginLeft: 1, marginTop: 1 }} onClick={handleMediaOpen} className={ProfileInformationComponentStyle.editButton}>
-                    Edit Image
-                </Button>
-                {openImage &&
-                    <Modal
-                        open={openImage}
-                        onClose={handleMediaClose}
-                        aria-labelledby="modal-modal-title"
-                        aria-describedby="modal-modal-description"
-                    >
-                        <Box sx={style}>
-                            Update profile picture
-                            <Media onClose={handleMediaClose} photoType={"profilePhoto"} />
-                        </Box>
-                    </Modal>
-                }
+                    <Button variant="outlined" sx={{ marginLeft: 1, marginTop: 1 }} onClick={handleMediaOpen} className={ProfileInformationComponentStyle.editButton}>
+                        <div className={ProfileInformationComponentStyle.editIcon}><AssignmentIndIcon /></div>
+                        {!isMobile && 'Edit Image'}
+                    </Button>
+                    {openImage &&
+                        <Modal
+                            open={openImage}
+                            onClose={handleMediaClose}
+                            aria-labelledby="modal-modal-title"
+                            aria-describedby="modal-modal-description"
+                        >
+                            <Box sx={style}>
+                                Update profile picture
+                                <Media onClose={handleMediaClose} photoType={"profilePhoto"} />
+                            </Box>
+                        </Modal>
+                    }
 
-                <Button variant="outlined" sx={{ marginLeft: 1, marginTop: 1 }} onClick={handleBannerOpen} className={ProfileInformationComponentStyle.editButton}>
-                    Edit Banner
-                </Button>
-                {openBanner &&
-                    <Modal
-                        open={openBanner}
-                        onClose={handleBannerClose}
-                        aria-labelledby="modal-modal-title"
-                        aria-describedby="modal-modal-description"
-                    >
-                        <Box sx={style}>
-                            Update Banner Image
-                            <Media onClose={handleBannerClose} photoType={"bannerPhoto"} />
-                        </Box>
-                    </Modal>
-                }
-            </div>
-        }
+                    <Button variant="outlined" sx={{ marginLeft: 1, marginTop: 1 }} onClick={handleBannerOpen} className={ProfileInformationComponentStyle.editButton}>
+                        <div className={ProfileInformationComponentStyle.editIcon}><AdUnitsIcon /></div>
+                        {!isMobile && 'Edit Banner'}
+                    </Button>
+                    {openBanner &&
+                        <Modal
+                            open={openBanner}
+                            onClose={handleBannerClose}
+                            aria-labelledby="modal-modal-title"
+                            aria-describedby="modal-modal-description"
+                        >
+                            <Box sx={style}>
+                                Update Banner Image
+                                <Media onClose={handleBannerClose} photoType={"bannerPhoto"} />
+                            </Box>
+                        </Modal>
+                    }
+                </div>
+            }
         </Box >
     );
 }
